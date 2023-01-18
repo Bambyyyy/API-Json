4f11cd1d645df09a6793127644f754bc32529be8
Jesper Flood <flod96@hotmail.com>
2023-01-18 18:27:46 +0100

    New update so thay you can see Altitude, Velocity and Visibility

--- src/api.ts
+++ src/api.ts
@@ -2,14 +2,24 @@ const btn = document.getElementById("button");
 const apiUrl = "https://api.wheretheiss.at/v1/satellites/25544";
 let la = document.getElementById("lat")!;
 let lo = document.getElementById("lon")!;
+let al = document.getElementById("alt")!;
+let vel = document.getElementById("vel")!;
+let vis = document.getElementById("vis")!;
 
 let previousValue1 = 0;
 let previousValue2 = 0;
+let previousValue3 = 0;
+let previousValue4 = 0;
+let previousValue5 = 0;
 
 btn?.addEventListener("click", function infoBox(event) {
   event.preventDefault();
   la.innerHTML = "";
   lo.innerHTML = "";
+  al.innerHTML = "";
+  vel.innerHTML = "";
+  vis.innerHTML = "";
+
   getInfoFromApi();
 });
 
@@ -18,9 +28,15 @@ async function getInfoFromApi() {
   const data = await response.json();
   la.append(`Latitude: ${data.latitude}`);
   lo.append(`Longitude: ${data.longitude}`);
+  al.append(`Altitude: ${data.altitude}`);
+  vel.append(`Velocity: ${data.velocity}`);
+  vis.append(`Visibility: ${data.visibility}`);
 
   previousValue1 = data.longitude;
   previousValue2 = data.latitude;
+  previousValue3 = data.altitude;
+  previousValue4 = data.velocity;
+  previousValue5 = data.visibility;
 }
 
 // async function getISS() {
